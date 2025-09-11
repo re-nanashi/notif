69c087a7ccff091c2f8b3e3f4f326ff6348ab721
re-nanashi <jfabro3124@gmail.com>
2025-09-11 12:41:53 +0800

    build: configure docker

---
+++ backend/src/main/java/com/notif/notif_api/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.notif.notif_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello world!";
+    }
+}
