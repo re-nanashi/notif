29002d288d3e4193bf7609db86ff635ad303f488
re-nanashi <jfabro3124@gmail.com>
2026-02-07 17:03:41 +0800

    chore: add constant for milliseconds to seconds conversion

--- backend/src/main/java/com/notif/api/common/util/Util.java
+++ backend/src/main/java/com/notif/api/common/util/Util.java
@@ -1,6 +1,8 @@
 package com.notif.api.common.util;
 
 public final class Util {
+    public static final long MILLISECONDS_PER_SECOND = 1000;
+
     public static boolean isNullOrBlank(String s) {
         return s == null || s.isBlank();
     }
