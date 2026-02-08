c9dd93c3fe461a7b1417b4e91724e7c1b814ac2c
re-nanashi <jfabro3124@gmail.com>
2026-02-08 10:29:18 +0800

    feat: basic app config impl

--- backend/src/main/java/com/notif/api/config/ApplicationConfig.java
+++ backend/src/main/java/com/notif/api/config/ApplicationConfig.java
@@ -1,13 +1,64 @@
 package com.notif.api.config;
 
+import com.notif.api.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class AppConfig {
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository userRepository;
+
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User with email '" + username + "' not found"));
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+    /**
+     * Authentication manager bean
+     * Required for programmatic authentication (e.g., in /generateToken)
+     *
+     * @param config the {@link AuthenticationConfiguration} that provides the
+     *               internally built {@link AuthenticationManager}
+     * @return the application-wide {@link AuthenticationManager}
+     * @throws Exception if the {@link AuthenticationManager} cannot be obtained
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
