30381466296921e9fc5a29ddf54833fe7112ea67
re-nanashi <jfabro3124@gmail.com>
2026-02-09 16:54:52 +0800

    feat: create AppConstants utility class

---
+++ backend/src/main/java/com/notif/api/common/constants/AppConstants.java
@@ -0,0 +1,9 @@
+package com.notif.api.common.constants;
+
+public final class AppConstants {
+    private AppConstants() {
+        throw new AssertionError("This class cannot be instantiated.");
+    }
+
+    public static final long MILLISECONDS_PER_SECOND = 1000;
+}
--- backend/src/main/java/com/notif/api/common/util/Util.java
+++ backend/src/main/java/com/notif/api/common/util/Util.java
@@ -1,8 +1,6 @@
 package com.notif.api.common.util;
 
 public final class Util {
-    public static final long MILLISECONDS_PER_SECOND = 1000;
-
     public static boolean isNullOrBlank(String s) {
         return s == null || s.isBlank();
     }
