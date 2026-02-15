53e08c0729bbc6a5d51cd86cf3e48f7b2fdb3349
re-nanashi <jfabro3124@gmail.com>
2026-02-15 21:02:26 +0800

    fix: fix password regex; special character

--- backend/src/main/java/com/notif/api/user/dto/CreateUserRequest.java
+++ backend/src/main/java/com/notif/api/user/dto/CreateUserRequest.java
@@ -28,7 +28,7 @@ public class CreateUserRequest {
     @NotBlank(message = "Password is required")
     @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
     @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{}|;:',.<>?/]).{8,64}$",
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{}|;:',.<>?/]).{8,64}$",
             message = "Password must contain uppercase, lowercase, number, and a special character"
     )
     private String password;
