d17037ca6b0af8b22485b6fc8f039a01a13dd139
re-nanashi <jfabro3124@gmail.com>
2026-02-15 20:57:16 +0800

    chore: add special characters for passwords

--- backend/src/main/java/com/notif/api/user/dto/CreateUserRequest.java
+++ backend/src/main/java/com/notif/api/user/dto/CreateUserRequest.java
@@ -27,8 +27,10 @@ public class CreateUserRequest {
     // TODO (Authentication): Will consider OAuth registration flow therefore password may not always be required.
     @NotBlank(message = "Password is required")
     @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,64}$",
-            message = "Password must contain uppercase, lowercase, and a number")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{}|;:',.<>?/]).{8,64}$",
+            message = "Password must contain uppercase, lowercase, number, and a special character"
+    )
     private String password;
 
     @NotBlank(message = "First name is required")
