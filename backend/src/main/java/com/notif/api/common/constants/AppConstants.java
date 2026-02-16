4ce26f8d922164be6cff13c55ae571cd8b71b2d9
re-nanashi <jfabro3124@gmail.com>
2026-02-16 15:38:26 +0800

    chore: add seconds per minutes

--- backend/src/main/java/com/notif/api/common/constants/AppConstants.java
+++ backend/src/main/java/com/notif/api/common/constants/AppConstants.java
@@ -8,5 +8,6 @@ public final class AppConstants {
     // Time conversions
     public static final long MILLISECONDS_PER_SECOND = 1000;
     public static final int MINUTES_PER_HOUR = 60;
+    public static final int SECONDS_PER_MINUTE = 60;
     public static final int HOURS_PER_DAY = 24;
 }
