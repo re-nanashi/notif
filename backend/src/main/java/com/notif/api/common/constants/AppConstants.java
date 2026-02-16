a44fa2edd1ae97b4ca1990ce31936eb00b6e76b3
re-nanashi <jfabro3124@gmail.com>
2026-02-16 11:22:58 +0800

    feat: add new time conversion constants

--- backend/src/main/java/com/notif/api/common/constants/AppConstants.java
+++ backend/src/main/java/com/notif/api/common/constants/AppConstants.java
@@ -5,5 +5,8 @@ public final class AppConstants {
         throw new AssertionError("This class cannot be instantiated.");
     }
 
+    // Time conversions
     public static final long MILLISECONDS_PER_SECOND = 1000;
+    public static final int MINUTES_PER_HOUR = 60;
+    public static final int HOURS_PER_DAY = 24;
 }
