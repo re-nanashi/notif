70aa19a2f02610c1901e0782b44b8bc8c1af3cd6
re-nanashi <jfabro3124@gmail.com>
2026-02-19 19:00:51 +0800

    feat: enable @Async

--- backend/src/main/java/com/notif/api/core/config/ApplicationConfig.java
+++ backend/src/main/java/com/notif/api/core/config/ApplicationConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableAsync
 @RequiredArgsConstructor
 public class ApplicationConfig {
     private final UserRepository userRepository;
