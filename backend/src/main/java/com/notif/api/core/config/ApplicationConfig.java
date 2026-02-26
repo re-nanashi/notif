4c269490cba285736767bbc50a7185b3424fd672
re-nanashi <jfabro3124@gmail.com>
2026-02-26 20:26:57 +0800

    refactor: configure custom UserDetailsService

--- backend/src/main/java/com/notif/api/core/config/ApplicationConfig.java
+++ backend/src/main/java/com/notif/api/core/config/ApplicationConfig.java
@@ -8,33 +8,18 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableAsync
 @RequiredArgsConstructor
 public class ApplicationConfig {
-    private final UserRepository userRepository;
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return userRepository.findByEmail(username)
-                        .orElseThrow(() -> new UsernameNotFoundException("User with email '" + username + "' not found"));
-            }
-        };
-    }
+    private final NotifUserDetailsService notifUserDetailsService;
     private final PasswordEncoder passwordEncoder;
 
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService());
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(notifUserDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
 
         return new ProviderManager(authenticationProvider);
