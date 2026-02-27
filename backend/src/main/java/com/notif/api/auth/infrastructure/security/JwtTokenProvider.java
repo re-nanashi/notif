3e1f1d913ce661674ef8343aa726fe9fb163f119
re-nanashi <jfabro3124@gmail.com>
2026-02-27 12:11:28 +0800

    chore: remove comment; refresh token will be opaque

--- backend/src/main/java/com/notif/api/auth/infrastructure/security/JwtTokenProvider.java
+++ backend/src/main/java/com/notif/api/auth/infrastructure/security/JwtTokenProvider.java
@@ -16,7 +16,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-// TODO: Refresh tokens
 @Service
 public class JwtTokenProvider {
     @Value("${jwt.secret}")
