4e1a301ff9c2d2d6ed1b7b5f7d9692d5375d90f5
re-nanashi <jfabro3124@gmail.com>
2026-02-28 01:15:15 +0800

    chore: remove unused import

--- backend/src/main/java/com/notif/api/auth/application/service/RefreshTokenServiceImpl.java
+++ backend/src/main/java/com/notif/api/auth/application/service/RefreshTokenServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 /**
